723b105d2ad49d234588a344bad25345528dc0ac
michael-delvin <d1041241057@gmail.com>
2026-02-13 15:13:21 +0700

    Latihan

---
+++ p041NamingConvention.java
@@ -0,0 +1,21 @@
+/*
+Nim: D1041241057
+Nama: Michael Delvin
+LATIHAN
+*/
+
+public class p041NamingConvention {
+
+    public static final double PI = 3.14;
+    public static void main(String[] args) {
+        // Variabel dengan camelCase
+        int studentAge = 20;
+        String studentName = "Budi";
+        boolean isActive = true;
+
+        System.out.println("Nama: " + studentName);
+        System.out.println("Umur: " + studentAge);
+        System.out.println("Status: " + isActive);
+        System.out.println("Konstanta PI: " + PI);
+    }
+}
